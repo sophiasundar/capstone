cde89dcf528a028ba844d01045a7856745d8854f
sophiasundar <sophiagifta17@gmail.com>
2024-01-28 14:16:02 +0530

    commit

--- src/components/DisplayItems.jsx
+++ src/components/DisplayItems.jsx
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from "axios";
+import ItemCard from './itemCard';
 
 
 
 export function DisplayItems(){
     
+    const [itemData,setItemData] = useState([])
     
+    useEffect(()=>{
     axios.get("https://65b4f8bd41db5efd28672348.mockapi.io/phones")
-    .then((res)=>console.log(res));
-    
-    
-    return<div>DisplayItems</div>
+    .then((res)=>{
+        if (res.status === 401){
+        console.log(" Data Not Found !")
+        }
+        console.log(res.data);
+        // data is only showing the objects
+        setItemData(res.data);
+    });
+ }, []);
+        console.log(itemData)
+    return (
+      <div>
+        <h1>DisplayItems</h1>
+        {itemData.map((item)=>{
+             return <ItemCard key={item.id}/>
+        })
+        }
+      </div>
+    );
 }
---
+++ src/components/itemCard.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+
+
+// "discountPercentage" "salesPercentage""rating"
+    function ItemCard(){
+        return(
+            <>
+               
+            </>
+        )
+    }
+
+export default ItemCard    
